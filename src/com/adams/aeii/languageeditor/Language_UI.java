d5c402cad7f8f5e2600f2b27ddd1eab02ad2db1e
s1407003 <s1407003@WSH-S1407003.wkscn.wistron>
2014-08-14 13:28:11 +0800

    language editor

---
+++ src/com/adams/aeii/languageeditor/Language_UI.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adams.aeii.languageeditor;
+
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author s1407003
+ */
+public class Language_UI extends JFrame {
+
+    private final Button btn;
+    private final Jt_Language jlg;
+    
+    public Language_UI() {
+        this.getContentPane().setPreferredSize(new Dimension(500,500));
+        this.pack();
+        this.setTitle("Language Editor");
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.getContentPane().setLayout(null);
+        
+        
+        jlg = new Jt_Language();
+        btn = new Button(this,jlg);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
+        this.getContentPane().add(jlg.initJtLanguage());
+        this.getContentPane().add(btn.initJpButton());
+        this.setVisible(true);
+        
+    }
+    
+    public static void main (String[] args) {
+        new Language_UI();
+    }
+}
